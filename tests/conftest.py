0209cb8e92a25848c9adaf96bff83c68bf3cdae4
Doug Lazenby <douglas.lazenby@ons.gov.uk>
2026-05-14 17:22:33 +0100

    Adding typing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+from pytest_mock import MockerFixture
 import requests
 
 @pytest.fixture
-def mock_session(mocker):
+def mock_session(mocker: MockerFixture) -> requests.Session:
     session = requests.Session()
     mocker.patch.object(session, "get")
     return session
