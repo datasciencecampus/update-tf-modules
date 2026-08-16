b771c69d6b494bd92c2be83f07e7d6e4e7b38836
pruthvi_patil <pruthvi0470@gmail.com>
2026-08-16 11:42:18 +0530

    replaced print statements with logging

--- src/update_tf_modules/updaters/registry_source.py
+++ src/update_tf_modules/updaters/registry_source.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+import logging
 import re
 
 from ..config import ROOT
+
+
+logger = logging.getLogger(__name__)
+
 
 def update_registry_module(
     file_path: Path,
@@ -90,7 +95,7 @@ def update_registry_module(
     old_content = file_path.read_text(encoding="utf-8")
     if new_content != old_content:
         file_path.write_text(new_content, encoding="utf-8")
-        print(
+        logger.info(
             f"Updated registry module '{source}' in {file_path.relative_to(ROOT)} to {new_version}"
         )
 
